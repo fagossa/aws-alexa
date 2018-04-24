ecd1980a09dd1cb5206bb5f29aa2d812f78fefbe
Fabian Gutierrez <fagossa@gmail.com>
2018-04-24 09:54:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// full code is here: https://glitch.com/edit/#!/info-trafic-alexa-skill?path=README.md:1:0
 var express = require("express");
 var alexa = require("alexa-app");
+var unirest = require('unirest');
+
 
 var PORT = process.env.port || 8080;
 var app = express();
@@ -7,8 +10,8 @@ var app = express();
 var alexaApp = new alexa.app("message/receive");
 
 var Phrases = {
-  "Launch"    :'Hello ! welcome to this TachLab',
-  "Help"      :'You ask for help',
+  "Launch"    :'<say-as interpret-as="interjection">Mystère mystère</say-as> Bonsoir, bienvenu dans la soirée Alexa',
+  "Help"      :'je suis là pour vous aidez, il suffit de me demander',
   "Stop"      :'You ask me to stop',
   "Error"     :"By the Hoary hosts of Hoggoth, I need you to try again.",
   "NotHeard"  :'What is it?  I suggest you try it again.',
@@ -20,29 +23,32 @@ var Phrases = {
 
 alexaApp.express({expressApp: app, debug: true});
 
-
 alexaApp.launch(function(request, res) {
-  res.say(Phrases.Launch).shouldEndSession(false).send();
+  res.say(Phrases.Launch).shouldEndSession(false);
 });
 
-// Exemple :
-/*
 alexaApp.intent("AMAZON.HelpIntent",
   function(req, res) {
-    res.say("Yes i'm here for that, ask me any things").shouldEndSession(false).send();
+    res.say(Phrases.help).shouldEndSession(false);
   }
 );
-*/
 
-// Exemple :
-/*
 alexaApp.intent('getInfoTrafic',
   function(req, res) {
-    console.log(JSON.stringify(req));
-    res.say("état du trafic").send();
+    var line = req.slots["metroLine"].value;
+  
+     return new Promise((resolve, reject) => {
+        unirest.get('https://www.ratp.fr/meteo/ajax/data')
+          .end(function (response) {
+            var message = response.body.status.metro.lines[line].message;
+            res.say("Voici l'état du trafic de la ligne " + line);
+            res.say(message).shouldEndSession(false);
+            resolve();
+          });
+      });  
+  
   }
 );
-*/
 
 app.listen(PORT);
 
